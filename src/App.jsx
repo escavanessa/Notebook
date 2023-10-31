37bb0d2b93af67e1e4aa11c30c00d343e5ab889b
vanessa <escavanessa@gmail.com>
2023-10-31 13:06:32 -0400

    i deleted everything becuse i failed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+//a form for the input and a ul for the tasks.
+//set up state for the item so that way it is recorded
+//add onsubmit handlesubmit
+//set up state to take the recorded item and add it to todo list
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+
+function App() {
+  return(
+    <h1>why are you so stupid
+    </h1>
   )
 }
-
+ 
 export default App
