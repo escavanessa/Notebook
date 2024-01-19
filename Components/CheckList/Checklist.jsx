6d658ca21442a2ef863fe6657b06c3e7d9bbeecc
vanessa <escavanessa@gmail.com>
2024-01-18 21:03:15 -0500

    add delete todo logic.

--- Components/CheckList/Checklist.jsx
+++ Components/CheckList/Checklist.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import { useState } from 'react'
+
+
 
 const Checklist = () => {
+    const [input, setInput] = useState('');
+    const [todo, setTodo] = useState([]);
+    
+    function handleChange(e) {
+        setInput(e.target.value)
+    }
+
+    function handleAdd() {
+        if (input === '') {
+            return;
+        }
+        setTodo([...todo, input])
+        setInput('');
+    }
+
+    function handleDelete() {
+
+    }
+
   return (
-    <div>Checklist</div>
+    <>
+    <h1>CheckList!</h1>
+    <input
+    placeholder='new item'
+    value={input}
+    onChange={handleChange}
+    />
+    <button
+    onClick={handleAdd}>Add</button>
+
+    <ul>
+        {todo.map((todo, index) => (
+            <li key={index}>{todo}
+            <button onClick={handleDelete}>Delete</button>
+            </li>
+        ))}
+    </ul>
+    </>
   )
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ import Checklist from '../Components/CheckList/Checklist'
 import StickyNote from '../Components/StickyNote/StickyNote'
 import './App.css'
 
-//a form for the input and a ul for the tasks.
-//set up state for the item so that way it is recorded
-//add onsubmit handlesubmit
-//set up state to take the recorded item and add it to todo list
-
 
 function App() {
   return (
